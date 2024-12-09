4a73e94544625a6753ec1881672e698c0b193df3
Fathya Azra Nazmira 039 <108808542+FathyaAzra@users.noreply.github.com>
2024-12-09 09:15:21 +0700

    Inisialisasi Profile

--- app/src/main/java/net/fazra/pmsejati/Activity/DashboardActivity.kt
+++ app/src/main/java/net/fazra/pmsejati/Activity/DashboardActivity.kt
@@ -1,17 +1,32 @@
 package net.fazra.pmsejati.Activity
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import net.fazra.pmsejati.R
+import androidx.recyclerview.widget.GridLayoutManager
+import net.fazra.pmsejati.Adapter.OngoingAdapter
+import net.fazra.pmsejati.ViewModel.MainViewModel
+import net.fazra.pmsejati.databinding.ActivityMainBinding
 
 class DashboardActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root) // Use binding.root instead of layout ID
+
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        val ongoingAdapter = OngoingAdapter(mainViewModel.loadData())
+
+        binding.viewOngoing.apply {
+            adapter = ongoingAdapter
+            layoutManager = GridLayoutManager(this@DashboardActivity, 2)
+        }
     }
 }
--- app/src/main/java/net/fazra/pmsejati/Repository/MainRepository.kt
+++ app/src/main/java/net/fazra/pmsejati/Repository/MainRepository.kt
@@ -1,5 +1,7 @@
 package net.fazra.pmsejati.Repository
 
+import net.fazra.pmsejati.Domain.OngoingDomain
+
 class MainRepository {
     val items = listOf(
         OngoingDomain("Area15 Medley", "April 15, 2024", 20, "ongoing1"),
